b04e30be85daf217762e476cddc46fe2b708620b
Anderson Grudtner Martins <anderson@alledia.com>
2018-08-08 10:08:21 -0300

    Update version

--- src/library/Container.php
+++ src/library/Container.php
@@ -7,7 +7,7 @@ class Container extends \Pimple\Container {
         parent::__construct( $values );
 
         $this['VERSION'] = function ( $c ) {
-            return '0.2.0';
+            return '0.2.1';
         };
 
         $this['PLUGIN_BASENAME'] = function ( $c ) use ( $values ) {
